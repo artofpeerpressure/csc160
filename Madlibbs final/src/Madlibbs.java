2baf12c42cc87356e12d05a66c13e4b3a5e79a7a
artofpeerpressure <barracktr@s.dcsdk12.org>
2021-03-03 13:18:41 -0700

    bruh

--- Madlibbs final/src/Madlibbs.java
+++ Madlibbs final/src/Madlibbs.java
@@ -5,15 +5,52 @@ public class Madlibbs {
             Scanner myObj = new Scanner(System.in);
             String noun;
             String adj;
-
+            String verb;
+            String noun2;
+            String adj2;
+            String verb2;
+            String noun3;
+            String adj3;
+            String verb3;
+            String noun4;
+            String adj4;
+            String verb4;
+            String noun5;
+            String adj5;
+            String verb5;
             // Enter username and press Enter
             System.out.println("Enter Noun");
             noun = myObj.nextLine();
             System.out.println("Enter Adjective");
             adj = myObj.nextLine();
+            System.out.println("Enter Verb");
+            verb = myObj.nextLine();
+            System.out.println("Enter Noun");
+            noun2 = myObj.nextLine();
+            System.out.println("Enter Adjective");
+            adj2 = myObj.nextLine();
+            System.out.println("Enter Verb");
+            verb2 = myObj.nextLine();
+            System.out.println("Enter Noun");
+            noun3 = myObj.nextLine();
+            System.out.println("Enter Adjective");
+            adj3 = myObj.nextLine();
+            System.out.println("Enter Verb");
+            verb3 = myObj.nextLine();
+            System.out.println("Enter Noun");
+            noun4 = myObj.nextLine();
+            System.out.println("Enter Adjective");
+            adj4 = myObj.nextLine();
+            System.out.println("Enter Verb");
+            verb4 = myObj.nextLine();
+            System.out.println("Enter Noun");
+            noun5 = myObj.nextLine();
+            System.out.println("Enter Adjective");
+            adj5 = myObj.nextLine();
+            System.out.println("Enter Verb");
+            verb5 = myObj.nextLine();
 
-
-            System.out.println("You are a "+adj+" "+noun);
+            System.out.println("You are a "+adj+" "+noun+"who "+verb+". You meet a "+adj2+" "+noun2+" who hurts "+adj3+" "+noun3+" by "+verb3+""+verb4+""+verb5+""+adj4+"" +adj5+""+noun4+""+noun5+" ");
         }
 
 }
