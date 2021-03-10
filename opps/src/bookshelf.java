e258a0bd09088c30a24db01edcb7e66f02c696ee
artofpeerpressure <barracktr@s.dcsdk12.org>
2021-03-10 13:18:01 -0700

    bruruurur

---
+++ opps/src/bookshelf.java
@@ -0,0 +1,12 @@
+public class bookshelf {
+    private static Book[] availableBooks = new Book[10];
+    public static void main(String[] args) {
+        populateAvailableBooks();
+        listAvailableBooks();
+        determineBookSelection();
+
+    }
+    public static void populateAvailableBooks(){
+        Book b1 = new Book("")
+    }
+}
